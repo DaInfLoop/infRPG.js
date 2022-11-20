dd3657bb9dda392eff2778af186ff534f02d5012
Banned User <92693892+DaInfLoop@users.noreply.github.com>
2022-11-20 17:56:12 +0000

    Forgor to change index.js to the main index.js of the lib

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const Client = require('./src/Client');
+module.exports = require('./src/Client')
 
-module.exports = Client;
-
-module.exports.Item = require('./src/Item');
-module.exports.Enemy = require('./src/Enemy');
-module.exports.User = require('./src/User');
+module.exports.Item = require('./src/Item')
+module.exports.Enemy = require('./src/Enemy')
+module.exports.User = require('./src/User')
+module.exports.Client = require('./src/Client')
